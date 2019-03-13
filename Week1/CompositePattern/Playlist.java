1eb625739e049d998cca3c88cde51bc59a47e22b
Abdulmaguid Eissa <abdulmaguid.eissa@gmail.com>
2019-03-13 19:02:54 +0200

    Add files via upload

---
+++ Week1/CompositePattern/Playlist.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+
+public class Playlist implements IComponent {
+    public String playlistName;
+    public ArrayList<IComponent> playlist = new ArrayList<>();
+
+    public Playlist(String playlistName){
+        this.playlistName = playlistName;
+    }
+
+    public void play(){
+        for (IComponent component : this.playlist){
+            component.play();
+        }
+    }
+
+    public void setPlaybackSpeed(float speed){
+        for (IComponent component : this.playlist){
+            component.setPlaybackSpeed(speed);
+        }
+    }
+
+    public String getName(){
+        return this.playlistName;
+    }
+
+    public void add(IComponent component){
+        this.playlist.add(component);
+    }
+
+    public void remove(IComponent component){
+        this.playlist.remove(component);
+    }
+
+}
